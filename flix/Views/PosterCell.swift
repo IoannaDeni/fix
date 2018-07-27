dae85f3b4775318b3ab2dfa2054770ab00043c28
user143365 <user143365@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-07-27 00:50:19 -0400

    all requierments completed

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by user143365 on 7/26/18.
+//  Copyright © 2018 Ioanna Deni. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
